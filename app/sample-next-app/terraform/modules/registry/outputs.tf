f3c6934388d29a632d89a3dc2060d48a4934c7ad
Kaustav <kaustav86x@gmail.com>
2026-07-30 21:56:37 +0000

    remove .terraform dir and add .gitignore

--- app/sample-next-app/terraform/modules/registry/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "container_name" {
-    description = "Name of the container"
-    value = docker_container.registry_container.name
-}
-
-output "functional_port" {
-    description = "Port the container is running on"
-    value = docker_container.registry_container.ports[0].external
-}
